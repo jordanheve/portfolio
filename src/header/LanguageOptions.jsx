7713f803bddf4d9c10bd647a38ba3ed12f630be5
Jordan Herrera Vera <jordan.herreravera@gmail.com>
2023-12-02 14:16:12 -0600

    updated language event for mobile compatibility

--- src/header/LanguageOptions.jsx
+++ src/header/LanguageOptions.jsx
@@ -2,21 +2,21 @@ import { useTranslation } from "react-i18next";
 
 export default function LanguageOptions() {
   const { t, i18n } = useTranslation();
-  const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng);
+
+  const changeLanguage = (event) => {
+    const selectedLanguage = event.target.value;
+    i18n.changeLanguage(selectedLanguage);
   };
+
   return (
     <label htmlFor="language">
       {t("Language")}:
       <select
         className="mt-1 cursor-pointer font-medium bg-teal-400 border border-teal-700 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        onChange={changeLanguage}
       >
-        <option onClick={() => changeLanguage("en")}>
-          English
-        </option>
-        <option onClick={() => changeLanguage("es")}>
-          Español
-        </option>
+        <option value="en">English</option>
+        <option value="es">Español</option>
       </select>
     </label>
   );
